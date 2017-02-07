2272274b4ae84971ee544d64851cac755e3fc96e
chtammik <christopher@tammik.ca>
2017-02-06 21:31:51 -0800

    Lesson 1: - MonoBehaviour basics - Ensuring a valid AudioSource - Setting basic source properties - storing and editing Prefabs
    Commit also contains various audio files for test usage.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # IntroToGameAudioScripting
+
+This repository hosts the source code for my "Intro to game audio scripting" articles.
+
+Licensed under a modified MIT licence which allows for commercial and non-commercial use but prohibits use in military applications. Be nice.
---
+++ UnityProject/Assets/Scripts/Audio/AudioSourceController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour {
+
+    private AudioSource _source;
+
+    public AudioClip Clip;
+    [Range(0,1)]
+    public float Volume = 1f;
+    [Range(.25f, 3)]
+    public float Pitch = 1f;
+    public bool Loop = false;
+    [Range(0f, 1f)]
+    public float SpacialBlend = 1f;
+
+    void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+        if(_source == null)
+        {
+            _source = gameObject.AddComponent<AudioSource>();
+        }
+    }
+    
+	void Start () {
+        Play();
+	}
+
+    public void SetSourceProperties(AudioClip clip, float volume, float picth, bool loop, float spacialBlend)
+    {
+        _source.clip = clip;
+        _source.volume = volume;
+        _source.pitch = picth;
+        _source.loop = loop;
+        _source.spatialBlend = spacialBlend;
+    }
+
+    public void Play()
+    {
+        SetSourceProperties(Clip, Volume, Pitch, Loop, SpacialBlend);
+        _source.Play();
+    }
+}
